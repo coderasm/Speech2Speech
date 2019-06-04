21615ffd9b9e6eb2319de90b8570e52b40211fdc
coderasm <coderasm@protonmail.com>
2019-06-04 16:50:48 -0700

    refactoring

---
+++ SpeechToSpeech/ViewModels/MainViewModel.cs
@@ -0,0 +1,74 @@
+﻿using SpeechToSpeech.Services;
+using SpeechToSpeech.Views;
+using System.IO;
+using Unity.Attributes;
+
+namespace SpeechToSpeech.ViewModels
+{
+  public class MainViewModel
+  {
+    private ISettingsService settingsService { get; set; }
+    private IDialogService dialogService { get; set; }
+    [Dependency]
+    public GoogleWebService googleWebService { get; set; }
+    [Dependency]
+    public AmazonWebService amazonWebService { get; set; }
+    [Dependency]
+    public IBMWebService ibmWebService { get; set; }
+    private Hotkey hotkeys;
+    private IAudioService audioService { get; set; }
+
+    public MainViewModel(ISettingsService settingsService, IAudioService audioService, IDialogService dialogService)
+    {
+
+      this.settingsService = settingsService;
+      this.dialogService = dialogService;
+      this.audioService = audioService;
+      DatabaseService.Initialize(settingsService.settings);
+      createFolders();
+    }
+
+    private void createFolders()
+    {
+      if (!Directory.Exists(@".\vocalized"))
+        Directory.CreateDirectory(@".\vocalized");
+      if (!Directory.Exists(@".\transcribed"))
+        Directory.CreateDirectory(@".\transcribed");
+    }
+
+    public void openSettingsDialog()
+    {
+      if (dialogService.ShowDialog<SettingsDialog>() == true)
+      {
+        settingsService.SaveSettings();
+      }
+    }
+
+    public async void vocalizeText(string text)
+    {
+      string audioFile = "";
+      var webServices = new ITranscribeAndVocalize<Voice>[]
+      {
+        googleWebService, amazonWebService, ibmWebService
+      };
+      var activeService = webServices[settingsService.settings.generalSettings.ActiveTextToSpeechService];
+      audioFile = await activeService.ToAudio(text);
+      if (audioFile != "")
+        playFile(audioFile);
+    }
+
+    private void playFile(string audioFileName)
+    {
+      hotkeys = Hotkey.Create(settingsService.settings.generalSettings.AppPush2TalkKey);
+      audioService
+        .OnPlay(() => hotkeys.BroadcastDown())
+        .OnPlayStopped(() => hotkeys.BroadcastUp())
+        .Play(audioFileName, settingsService.settings.generalSettings.AudioOutDevice);
+    }
+
+    public void stopAudio()
+    {
+      audioService.Stop();
+    }
+  }
+}
--- SpeechToSpeech/Views/MainWindow.xaml.cs
+++ SpeechToSpeech/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using NAudio.Wave;
 using SpeechToSpeech.Services;
+using SpeechToSpeech.ViewModels;
 using System.IO;
 using System.Windows;
 using Unity.Attributes;
@@ -11,68 +12,31 @@ namespace SpeechToSpeech.Views
   /// </summary>
   public partial class MainWindow : Window
   {
-    private ISettingsService settingsService { get; set; }
-    private IDialogService dialogService { get; set; }
     [Dependency]
-    public GoogleWebService googleWebService { get; set; }
-    [Dependency]
-    public AmazonWebService amazonWebService { get; set; }
-    [Dependency]
-    public IBMWebService ibmWebService { get; set; }
-    private Hotkey hotkeys;
-    private IAudioService audioService { get; set; }
-
-    public MainWindow(ISettingsService settingsService, IAudioService audioService, IDialogService dialogService)
+    public MainViewModel ViewModel
     {
-      InitializeComponent();
-      this.settingsService = settingsService;
-      this.dialogService = dialogService;
-      this.audioService = audioService;
-      DatabaseService.Initialize(settingsService.settings);
-      createFolders();
+      set { DataContext = value; }
+      get { return (MainViewModel)DataContext; }
     }
 
-    private void createFolders()
+    public MainWindow()
     {
-      if (!Directory.Exists(@".\vocalized"))
-        Directory.CreateDirectory(@".\vocalized");
-      if (!Directory.Exists(@".\transcribed"))
-        Directory.CreateDirectory(@".\transcribed");
+      InitializeComponent();
     }
 
     private void OnOptionsClicked(object sender, RoutedEventArgs e)
     {
-      if (dialogService.ShowDialog<SettingsDialog>() == true)
-      {
-        settingsService.SaveSettings();
-      }
+      ViewModel.openSettingsDialog();
     }
 
-    private async void sendTextButton_Click(object sender, RoutedEventArgs e)
+    private void sendTextButton_Click(object sender, RoutedEventArgs e)
     {
-      string audioFile = "";
-      var webServices = new ITranscribeAndVocalize<Voice>[]
-      {
-        googleWebService, amazonWebService, ibmWebService
-      };
-      var activeService = webServices[settingsService.settings.generalSettings.ActiveTextToSpeechService];
-      audioFile = await activeService.ToAudio(textToSendBox.Text);
-      if (audioFile != "")
-        playFile(audioFile);
-    }
-
-    private void playFile(string audioFileName)
-    {
-      hotkeys = Hotkey.Create(settingsService.settings.generalSettings.AppPush2TalkKey);
-      audioService
-        .OnPlay(() => hotkeys.BroadcastDown())
-        .OnPlayStopped(() => hotkeys.BroadcastUp())
-        .Play(audioFileName, settingsService.settings.generalSettings.AudioOutDevice);
+      ViewModel.vocalizeText(textToSendBox.Text);
     }
 
     private void OnButtonStopClick(object sender, StoppedEventArgs args)
     {
-      audioService.Stop();
+      ViewModel.stopAudio();
     }
   }
 }
